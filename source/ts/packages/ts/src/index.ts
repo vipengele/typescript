7e18698fc3a365a22ce64cb9df45c4da6c4813b4
Pedro Gomes <pedromvgomes@hotmail.com>
2026-09-26 09:23:56 +0100

    feat(ts): re-export VipengeleError and the Attributes surface from ts-core-common

--- source/ts/packages/ts/src/index.ts
+++ source/ts/packages/ts/src/index.ts
@@ -1,3 +1,6 @@
 export { Numeric } from "@vipengele/ts-core-common/types/numeric";
 export { redact } from "@vipengele/ts-core-redaction";
 export type { RedactionPolicy, KeyMatcher, RedactOptions, Replacement } from "@vipengele/ts-core-redaction";
+export { normalizeAttributes } from "@vipengele/ts-core-common/attributes";
+export type { AttributeValue, Attributes, AttributesInput, NormalizeAttributesOptions } from "@vipengele/ts-core-common/attributes";
+export { VipengeleError } from "@vipengele/ts-core-common";
